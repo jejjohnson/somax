7a9b5e56c63c195706db9186cd2d73df09266e84
Takaya Uchida <tuchida@bfs-v13-skynet.coaps.fsu.edu>
2023-12-30 15:14:09 -0500

    Free slip harmonic dissipation

--- somax/_src/models/qg/params.py
+++ somax/_src/models/qg/params.py
@@ -7,7 +7,7 @@ class QGParams(eqx.Module):
     tau0: float = 2.0e-5  # wind stress magnitude m/s^2
     y0: float = 2_400_000.0  # [m]
     a_2: float = 0.0  # laplacian diffusion coef (m^2/s)
-    a_4: float = 5.0e11  # LR # 2.0e9 # HR
+    a_4: float = 0.0  # biharmonic diffusion coef (m^4/s)
     bcco: float = 0.2  # boundary condition coef. (non-dim.)
     delta_ek: float = 2.0  # eckman height [m]
     num_pts: int = 5
